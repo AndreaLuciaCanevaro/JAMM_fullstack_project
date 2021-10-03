9d1ef4abca68b0a185f447e56bc7a46e6ff4a7e0
AndreaCanevaro <andrea.canevaro.f@gmail.com>
2021-10-03 20:20:07 -0300

    ordeno archivo app.js

--- Jamm/src/app.js
+++ Jamm/src/app.js
@@ -1,35 +1,30 @@
+//CONST Y REQUIRE
 const express = require('express');
-const app = express();
+const app = express(); 
 const path = require('path');
 const methodOverride = require('method-override');
 const session = require ('express-session');
-
-app.set("view engine" , "ejs");
-app.set("public", path.join(__dirname, "public"));
-app.set('views', path.join(__dirname, 'views'));
-
-const mainRoutes = require ("./routes/mainRoutes");
+const mainRoutes = require ("./routes/mainRoutes"); 
 const productsRoutes = require ("./routes/productsRoutes");
 const usersRoutes = require ("./routes/usersRoutes");
 
+//USE
 app.use(express.static ("../public"));
-
+app.use(methodOverride('_method'));
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
-app.use(methodOverride('_method'));
+//VER CÓMO FUNCIONA ESTO
 app.use(session({secret:'Esto es un secreto'}));
-
-//revisar ruta de mierda
+//revisar ruta
 app.use("/", mainRoutes);
 app.use("/", productsRoutes);
 app.use("/", usersRoutes);
 
+//SET
+app.set("view engine" , "ejs");
+app.set("public", path.join(__dirname, "public"));
+app.set('views', path.join(__dirname, 'views'));
 
 app.listen(3010, ()=>{
     console.log('Servidor funcionando');
 });
-
-
-
-
-
