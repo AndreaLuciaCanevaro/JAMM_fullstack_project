efbe7275f81b4845b2f49372171f35f3cc337b57
AndreaCanevaro <andrea.canevaro.f@gmail.com>
2021-10-06 16:53:22 -0300

    revisión y comentarios en utheMiddleware

--- Jamm/src/middlewares/authMiddleware.js
+++ Jamm/src/middlewares/authMiddleware.js
@@ -1,3 +1,5 @@
+//Ver si el usuario está logueado en session y redirect a vista users/login
+//Si no está logueado, next()
 function authMiddleware (req, res, next) {
     if (!req.session.userLogged){
         return res.redirect ('/users/login')
