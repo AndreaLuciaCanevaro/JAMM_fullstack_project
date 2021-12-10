14e42abb4e5eb09bdc329f9346632c0a219ea171
Mmartintdd <taddiamartin@gmail.com>
2021-12-10 15:10:10 -0300

    mas correcciones Dashboard

--- React/dashboard-jamm/src/components/ContentRowTop.js
+++ React/dashboard-jamm/src/components/ContentRowTop.js
@@ -2,8 +2,21 @@ import React from 'react';
 import imagenFondo from '../assets/images/mandalorian.jpg';
 import CategoriesInDb from './CategoriesInDb';
 import ContentRowCards from './ContentRowCards';
+import {useState, useEffect} from 'react'
 
 function ContentRowTop(){
+
+	const [products, setProducts] = useState([])
+	const getProducts = async () => {
+	  await fetch('http://localhost:3010/api/products/18') // cambiar API
+		.then((response) => response.json())
+		.then((data) => setProducts(data))
+	}
+	useEffect(() => {
+	  getProducts()
+	}, [])
+
+	
     return(
         <React.Fragment>
 				{/*<!-- Content Row Top -->*/}
@@ -21,19 +34,19 @@ function ContentRowTop(){
 					<div className="row">
 						{/*<!-- Last Movie in DB -->*/}
 						<div className="col-lg-6 mb-4">
-							<div className="card shadow mb-4">
-								<div className="card-header py-3">
-									<h5 className="m-0 font-weight-bold text-gray-800">Último producto creado:</h5>
-								</div>
-								<div className="card-body">
-									<div className="text-center">
-										<img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: 40 +'rem'}} src={imagenFondo} alt=" Star Wars - Mandalorian "/>
-									</div>
-									<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, consequatur explicabo officia inventore libero veritatis iure voluptate reiciendis a magnam, vitae, aperiam voluptatum non corporis quae dolorem culpa citationem ratione aperiam voluptatum non corporis ratione aperiam voluptatum quae dolorem culpa ratione aperiam voluptatum?</p>
-									<a className="btn btn-danger" target="_blank" rel="nofollow" href="/">View movie detail</a>
-								</div>
-							</div>
-						</div>
+            				<div className="card shadow mb-4">
+                				<div className="card-header py-3">
+                    				<h5 className="m-0 font-weight-bold text-gray-800">Último producto creado: {products.product}</h5> 
+                				</div>
+                				<div className="card-body">
+                    				<div className="text-center">
+                        				<img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: 40 +'rem'}} src={products.image} alt={products.productName}/>
+                    				</div>
+                    				<p>Descripción: {products.description}</p>					
+									<p>Precio: ${products.price}</p>
+                				</div>
+            				</div>
+        				</div>
 						{/*<!-- End content row last movie in Data Base -->*/}
 
 						{/*<!-- Genres in DB -->*/}
